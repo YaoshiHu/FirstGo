0716f0b32fcd4edf04414aadfefb47548eb0a3e5
Yaoshi <henryhustone@outlook.com>
2018-05-28 23:06:46 -0400

    Handle panic

--- second.go
+++ second.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Printf("Hello World, Golang")
+	f()
+	fmt.Printf("End of Program")
+}
+
+func f() {
+	defer func() {
+		fmt.Println("Defer start")
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Defer end")
+	}()
+	for {
+		fmt.Println("Start of f()")
+		a := []string {"a", "b"}
+		fmt.Println(a[3])
+		panic("Fake panic")
+		fmt.Println("End of f()")
+	}
 }
